ded3d3c6f9f0e2908005ee1048f05a9924c91ad8
VICTOR LUIZ MENDONÇA DOS SANTOS <2022001518@ifam.edu.br>
2024-01-03 18:02:04 -0400

    feat: logo, couple and text into UI
    with constrains

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -9,15 +9,34 @@ import UIKit
 
 class HomeViewController: UIViewController {
    
+    // MARK: - UI Components
+    
+    private lazy var logoImage: UIImageView = {
+        let image = UIImageView(image: UIImage.logo)
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var coupleImage: UIImageView = {
+        let image = UIImageView(image: UIImage.couple)
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     private lazy var titleText: UILabel = {
         let label = UILabel()
-        label.text = "Hello World"
+        label.text = "O lugar ideal para buscar, salvar e organizar seus filmes favoritos!"
         label.textColor = .white
-        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
+    // MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .accent
@@ -26,13 +45,28 @@ class HomeViewController: UIViewController {
     }
     
     private func addViews() {
+        view.addSubview(logoImage)
+        view.addSubview(coupleImage)
         view.addSubview(titleText)
     }
     
     private func setConstrains() {
         NSLayoutConstraint.activate([
+            // Logo Cinetopia
+            logoImage.heightAnchor.constraint(equalToConstant: 102),
+            logoImage.widthAnchor.constraint(equalToConstant: 172),
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 79),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // Couple Image
+            coupleImage.heightAnchor.constraint(equalToConstant: 289),
+            coupleImage.widthAnchor.constraint(equalToConstant: 302),
+            coupleImage.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 24),
+            coupleImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // Title
+            titleText.topAnchor.constraint(equalTo: coupleImage.bottomAnchor, constant: 24),
             titleText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleText.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            titleText.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleText.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
         ])
     }
 }
