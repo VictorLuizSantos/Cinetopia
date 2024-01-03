f0594ddf6ac336226325edc8d6a6735fdddd74aa
VICTOR LUIZ MENDONÇA DOS SANTOS <2022001518@ifam.edu.br>
2024-01-02 23:52:42 -0400

    feat: ViewCode setup

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Cinetopia
 //
 //  Created by Victor Luiz on 02/01/24.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
 
