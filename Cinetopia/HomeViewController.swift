3e64a820e8bdcdffb531bff87d2fe7bbe8b1b4d7
VICTOR LUIZ MENDONÇA DOS SANTOS <2022001518@ifam.edu.br>
2024-01-03 00:44:49 -0400

    chore: encapsulating code

--- Cinetopia/HomeViewController.swift
+++ Cinetopia/HomeViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+   
+    private lazy var titleText: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .accent
+        addViews()
+        setConstrains()
     }
-
-
+    
+    private func addViews() {
+        view.addSubview(titleText)
+    }
+    
+    private func setConstrains() {
+        NSLayoutConstraint.activate([
+            titleText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleText.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
-
